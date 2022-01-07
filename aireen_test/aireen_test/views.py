cfd63b422c68a55a9ad409dc4780a5157728fbe2
Edelstadt <marcus.scalpere@gmail.com>
2022-01-07 17:15:46 +0100

    Initial

--- aireen_test/aireen_test/views.py
+++ aireen_test/aireen_test/views.py
@@ -2,71 +2,74 @@ from django.views.generic import TemplateView
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib import messages
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 class IndexView(TemplateView):
     template_name = "index.html"
+    queue1 = 'queue1'
+    queue2 = 'queue2'
+    queue3 = 'queue3'
 
-    def get(self, request, *args, **kwargs):
-        context = {
-            'queue1': ['user1', 'user2', 'user3'],
-            'queue2': ['user4', 'user5', 'user6'],
-            'queue3': ['user7', 'user8', 'user9'],
+    test_initial_data = {
+            queue1: ['user1', 'user2', 'user3'],
+            queue2: ['user4', 'user5', 'user6'],
+            queue3: ['user7', 'user8', 'user9'],
         }
 
-        return self.render_to_response(context)
+    queues = [
+        queue1,
+        queue2,
+        queue3,
+    ]
+
+    def get(self, request, *args, **kwargs):
+        return self.render_to_response(self.test_initial_data)
+
+    def _clean_queue(self, request):
+        return (
+            [x for x in request.POST[queue].split(" ") if x]
+            for queue in self.queues
+        )
 
     @staticmethod
-    def _process_queue(queue):
-        return [x for x in queue.split(" ") if x]
+    def _logger(user, queue, message):
+        logger.info(message)
 
-    @staticmethod
-    def logger(user, queue, message):
-        pass
+    def get_context_data(self, queues, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if len(self.queues) == len(queues):
+            context = dict(zip(self.queues, queues))
+        return context
 
     def post(self, request, *args, **kwargs):
-        queue_1 = self._process_queue(request.POST['queue1'])
-        queue_2 = self._process_queue(request.POST['queue2'])
-        queue_3 = self._process_queue(request.POST['queue3'])
+        queue_1, queue_2, queue_3 = self._clean_queue(request)
         name = request.POST.get('name')
 
         if name and request.POST.get('add'):
-            self.logger(name, 1, f"User {name} added to queue 1")
+            self._logger(name, 1, f"User {name} added to queue 1")
             queue_1.insert(0, name)
-
-            context = {
-                'queue1': queue_1,
-                'queue2': queue_2,
-                'queue3': queue_3,
-            }
         
         elif request.POST.get('queue1-remove'):
             name = queue_1[-1]
-            self.logger(name, 1, f"User {name} moved to queue 2")
+            self._logger(name, 1, f"User {name} moved to queue 2")
             queue_2.insert(0, name)
-
-            context = {
-                'queue1': queue_1[:-1],
-                'queue2': queue_2,
-                'queue3': queue_3,
-            }
+            del queue_1[-1]
         
         elif request.POST.get('queue2-remove'):
             name = queue_2[-1]
-            self.logger(name, 1, f"User {name} moved to queue 2")
+            self._logger(name, 1, f"User {name} moved to queue 2")
             queue_3.insert(0, name)
-
-            context = {
-                'queue1': queue_1,
-                'queue2': queue_2[:-1],
-                'queue3': queue_3,
-            }
+            del queue_2[-1]
 
         elif request.POST.get('queue3-remove'):
-            messages.info(request, 'User removed from queues.')
-            context = {
-                'queue1': queue_1,
-                'queue2': queue_2,
-                'queue3': queue_3[:-1],
-            }
+            name = queue_3[-1]
+            del queue_3[-1]
+            messages.info(request, f'User {name} removed from queues.')
+            self._logger(name, 1, f'User {name}removed from queues.')
 
-        return self.render_to_response(context)
+        return self.render_to_response(self.get_context_data([queue_1, queue_2, queue_3]))
